d98ea22bf5dfdae7fb3ea3fe1c63a1c9e53127e8
Tatsuhiko Seki <osekiharn@gmail.com>
2021-07-20 07:15:53 +0900

    動くように修正

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-const fs = require('fs');
-const { downloadImage, composite, resize, saveToS3 } = require('./utils');
+const { downloadImage, composite, resize, remove } = require('./utils');
 
-const bucket = 'lambda-images-bucket';
-
-const img = 'https://img.fumumu.net/wp-content/uploads/2020/02/fumumu20200227yoshitakayuriko1-1200x800.jpg'
-
-exports.handler = (async () => {
+exports.handler = async (event) => {
   try {
-    const buf = await downloadImage(img);
+    const buf = await downloadImage(event.url);
     const composited = await composite(buf);
-    // console.log(composited);
-    const resized = await resize(composited, 200);
-    fs.writeFileSync('./secondDemo.png', resized);
-    // const key = await saveToS3(bucket, event.name, resized);
-    // return { key };
+    const resized = await resize(composited, 300);
 
-    // const response = {
-    //   statusCode: 200,
-    //   headers: {
-    //       'Content-Type': 'application/json',
-    //       'Access-Control-Allow-Origin': '*' 
-    //   },
-    //   body: resized,
-    // };
-    // return response;
+    const response = {
+      statusCode: 200,
+      headers: {
+          'Content-Type': 'image/png',
+          'Access-Control-Allow-Origin': '*' 
+      },
+      body: resized,
+    };
+    return response;
   } catch(e) {
     console.error(e)
   }
-})();
+}
--- utils.js
+++ utils.js
@@ -1,8 +1,5 @@
 const GM = require('gm');
 const gm = GM.subClass({ imageMagick: true });
-const FileType = require('file-type');
-// const AWS = require('aws-sdk');
-// const s3 = new AWS.S3();
 const axios = require('axios');
 
 exports.downloadImage = async (url) => {
@@ -19,34 +16,32 @@ exports.resize = async (buf, width, height) => {
 const getBufferSize = (buf) => {
   return new Promise((resolve, reject) => {
     gm(buf)
-      .size((error, size) => {
-        const lgtmPngWidth = Math.floor(size.width * 0.56);
-        const centerWidth = Math.floor(size.width / 2);
-        const centerHeight = Math.floor(size.height / 2);
-        return error ? reject(error) : resolve({
-          lgtmPngWidth,
-          centerWidth,
-          centerHeight
-        })
+      .size((error, { width, height }) => {
+        return error ?
+          reject(error) : resolve({ width, height })
       })
   })
 }
 
-const resizeLgtm = (lgtmPngWidth) => {
-  const lgtmPngPath = './lgtm.png';
-  return new Promise((resolve, reject) => {
-    gm(lgtmPngPath)
-      .resize(lgtmPngWidth)
-      .write('./tmp/resized.png', (err) => {
-        return resolve()
-        // return err ? reject(err) : resolve(resizedLgtmBuffer)
-      });
-  })
+const getSelectLgtm = (width) => {
+  if (width >= 1000) {
+    return './assets/lgtm700.png'
+  } else if (width < 1000 && width >= 800) {
+    return './assets/lgtm500.png'
+  } else if (width < 800 && width >= 500) {
+    return './assets/lgtm300.png'
+  } else if (width < 500 && width >= 300) {
+    return './assets/lgtm150.png'
+  } else if (width < 300) {
+    return './assets/lgtm100.png'
+  }
 }
 
-const getGeometry = (centerWidth, centerHeight) => {
+const getGeometry = (lgtmPath, width, height) => {
   return new Promise((resolve, reject) => {
-    gm('./tmp/resized.png').size((error, size) => {
+    gm(lgtmPath).size((error, size) => {
+      const centerWidth = width / 2;
+      const centerHeight = height / 2;
       const left = Math.floor(centerWidth - (size.width / 2));
       const top = Math.floor(centerHeight - (size.height / 2));
       const geometry = '+' + left + '+' + top;
@@ -55,12 +50,10 @@ const getGeometry = (centerWidth, centerHeight) => {
   });
 }
 
-const getCompositedBuffer = (buf, geometry) => {
-  console.log(gm(buf));
-
+const getCompositedBuffer = (buf, lgtmPath, geometry) => {
   return new Promise((resolve, reject) => {
     return gm(buf)
-      .composite('./tmp/resized.png')
+      .composite(lgtmPath)
       .geometry(geometry)
       .quality(100)
       .noProfile()
@@ -72,25 +65,14 @@ const getCompositedBuffer = (buf, geometry) => {
 
 exports.composite = async (buf) => {
   try {
-    const { lgtmPngWidth, centerWidth, centerHeight } = await getBufferSize(buf) 
-    // const resizedLgtmBuffer = await resizeLgtm(lgtmPngWidth)
-    await resizeLgtm(lgtmPngWidth)
-    const geometry = await getGeometry(centerWidth, centerHeight);
-    return await getCompositedBuffer(buf, geometry);
+    const { width, height } = await getBufferSize(buf) 
+    console.log(width);
+    const lgtmPath = getSelectLgtm(width)
+    console.log(lgtmPath);
+    const geometry = await getGeometry(lgtmPath, width, height);
+    return await getCompositedBuffer(buf, lgtmPath, geometry);
   } catch(e) {
     console.log(e);
   }
 }
 
-exports.saveToS3 = async (bucket, name, buf) => {
-  const contentType = await FileType.fromBuffer(buf);
-  const key = `${name}.${contentType.ext}`;
-  await s3.putObject({
-    Bucket: bucket,
-    Key: key,
-    Body: buf,
-    ContentEncoding: 'base64',
-    ContentType: contentType.mime,
-  }).promise();
-  return key;
-};
